2722353343d80f30322c95b15c97f8230071ffc2
Vitoria Neri <vitoria.vneri@gmail.com>
2023-07-24 21:35:06 -0300

    alteracao LoginPage com if ternario

--- web/cypress/support/pages/LoginPage.js
+++ web/cypress/support/pages/LoginPage.js
@@ -12,17 +12,11 @@ class LoginPage {
 
     fill(user) {
 
-        if (user.email) {
-            cy.get('input[name=email]')
-                .clear()
-                .type(user.email)
-        }
+        cy.get('input[name=email]').clear({force: true}).as('email')
+        cy.get('input[name=password]').clear({force: true}).as('password')
 
-        if (user.password) {
-            cy.get('input[name=password]')
-                .clear()
-                .type(user.password)
-        }
+        user.email ? cy.get('@email').type(user.email) : cy.log('empty email')
+        user.password ? cy.get('@password').type(user.password) : cy.log('empty password')
     }
 
     submit() {
